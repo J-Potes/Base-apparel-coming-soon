0e1dae134682e0caaa6872d597c4b592ccf16b54
J-Potes <jpotes.multimedia@gmail.com>
2023-03-09 10:28:04 -0500

    input validation attempts

---
+++ script.js
@@ -0,0 +1,12 @@
+// document.querySelector('form').addEventListener('submit', function(event) {
+//     var emailInput = document.querySelector('#email');
+    
+//     if (!emailInput.validity.valid) {
+//       emailInput.style.border = '2px solid red';
+//       event.preventDefault();
+//     }
+//   });
+
+// document.querySelectorAll('input__container[data-error] .input__email').forEach(inpEl => {
+//     inpEl.addEventListener('input__email', () => inpEl.parentElement.removeAttribute('data-error'));
+// });
