cc3aec92e7237b41e8ca5184cfae4ad7ceda659e
J-Potes <jpotes.multimedia@gmail.com>
2023-03-13 22:45:06 -0500

    Email verification and hovers

--- script.js
+++ script.js
@@ -10,3 +10,9 @@
 // document.querySelectorAll('input__container[data-error] .input__email').forEach(inpEl => {
 //     inpEl.addEventListener('input__email', () => inpEl.parentElement.removeAttribute('data-error'));
 // });
+const form = document.querySelector('form');
+const submitButton = form.querySelector('button[type="submit"]');
+
+submitButton.addEventListener('click', (event) => {
+  form.classList.add('submitted');
+});
